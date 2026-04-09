6d50b8dd6c67033df49fdba1f125dae6a6f7df6e
Varsha V Nayak <varshavnayak2003@gmail.com>
2026-04-09 21:00:08 +0530

    Added js code

---
+++ VarshaNayak_ChatUI/js/chat.js
@@ -0,0 +1,5 @@
+const textarea = document.querySelector("textarea");
+const sendBtn = document.querySelector(".send-btn");
+const messages = document.querySelector(".messages");
+const typing = document.querySelector(".typing");
+const welcome = document.querySelector(".welcome");
